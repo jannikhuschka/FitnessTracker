151107090589582cc8a1df63932600facca0ec96
Jannik Huschka <huschka@neuland.dev>
2023-10-16 21:19:37 +0200

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Jannik Huschka on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
